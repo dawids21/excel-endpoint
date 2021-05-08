6d93d924ec96eca425232e1cf91220bf2bca1c08
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-05-08 17:21:21 +0200

    implement PoiResourceProvider using byte array streams

--- src/main/java/xyz/stasiak/excelendpoint/ExcelEndpointConfig.java
+++ src/main/java/xyz/stasiak/excelendpoint/ExcelEndpointConfig.java
@@ -8,6 +8,6 @@ class ExcelEndpointConfig {
 
     @Bean
     ResourceProvider resourceProvider() {
-        return new StaticResourceProvider();
+        return new PoiResourceProvider();
     }
 }
--- src/main/java/xyz/stasiak/excelendpoint/PoiResourceProvider.java
+++ src/main/java/xyz/stasiak/excelendpoint/PoiResourceProvider.java
@@ -1,5 +1,11 @@
 package xyz.stasiak.excelendpoint;
 
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.Optional;
 
@@ -7,6 +13,14 @@ class PoiResourceProvider implements ResourceProvider {
 
     @Override
     public Optional<InputStream> getResourceStream() {
-        return Optional.empty();
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        try (Workbook workbook = new XSSFWorkbook()) {
+            workbook.write(outputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        InputStream inputStream = new ByteArrayInputStream(outputStream.toByteArray());
+        return Optional.of(inputStream);
     }
 }
