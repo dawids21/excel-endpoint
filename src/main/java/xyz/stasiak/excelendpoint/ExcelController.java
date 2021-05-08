881089a303c1090159aa9de88c106ccc4e05eac3
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-05-08 11:32:35 +0200

    create controller for endpoint

---
+++ src/main/java/xyz/stasiak/excelendpoint/ExcelController.java
@@ -0,0 +1,10 @@
+package xyz.stasiak.excelendpoint;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+class ExcelController {
+
+}
