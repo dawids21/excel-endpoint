947671ce37b5af7a794143a8d0771874f12b6bf6
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-05-08 11:06:01 +0200

    initial commit

---
+++ src/main/java/xyz/stasiak/excelendpoint/ExcelEndpointApplication.java
@@ -0,0 +1,13 @@
+package xyz.stasiak.excelendpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelEndpointApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExcelEndpointApplication.class, args);
+    }
+
+}
---
+++ src/test/java/xyz/stasiak/excelendpoint/ExcelEndpointApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.stasiak.excelendpoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcelEndpointApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
