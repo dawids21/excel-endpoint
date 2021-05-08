d62aaa6284715d765ed29402ee4e301a5bdc179f
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-05-08 12:37:38 +0200

    add path variable to set filename

--- src/main/java/xyz/stasiak/excelendpoint/ExcelController.java
+++ src/main/java/xyz/stasiak/excelendpoint/ExcelController.java
@@ -4,6 +4,7 @@ import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
@@ -16,15 +17,15 @@ import java.util.Optional;
 @RequestMapping("/api")
 class ExcelController {
 
-    @GetMapping(value = "/excel", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
-    ResponseEntity<Resource> getWorkbook() throws IOException {
+    @GetMapping(value = "/excel/{filename}", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+    ResponseEntity<Resource> getWorkbook(@PathVariable String filename) throws IOException {
 
         InputStream resourceStream = getResourceStream().orElseThrow(
                  () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "File not found"));
         InputStreamResource resource = new InputStreamResource(resourceStream);
 
         HttpHeaders headers = new HttpHeaders();
-        headers.setContentDisposition(getContentDisposition("example-workbook.xlsx"));
+        headers.setContentDisposition(getContentDisposition(filename));
 
         return ResponseEntity.ok()
                              .contentLength(resourceStream.available())
