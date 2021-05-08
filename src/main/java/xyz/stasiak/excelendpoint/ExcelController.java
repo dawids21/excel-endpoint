c2d812677fc99b6ce1fc06dd25ea12b560c8706a
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-05-08 12:11:23 +0200

    add method to return static file

--- src/main/java/xyz/stasiak/excelendpoint/ExcelController.java
+++ src/main/java/xyz/stasiak/excelendpoint/ExcelController.java
@@ -1,10 +1,29 @@
 package xyz.stasiak.excelendpoint;
 
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 @RestController
 @RequestMapping("/api")
 class ExcelController {
+
+    @GetMapping(value = "/workbook", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+    ResponseEntity<Resource> getWorkbook() throws IOException {
+        InputStream resourceStream = getClass().getClassLoader()
+                                               .getResourceAsStream("static/example-workbook.xlsx");
+        InputStreamResource resource = new InputStreamResource(resourceStream);
+        return ResponseEntity.ok()
+                             .contentLength(resourceStream.available())
+                             .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                             .body(resource);
+    }
 
 }
