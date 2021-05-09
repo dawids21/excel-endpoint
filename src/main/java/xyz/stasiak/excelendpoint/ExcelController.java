b0255379b71e270adff272aa84c74a40a0223411
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-05-09 22:58:28 +0200

    use ByteArrayResource in ExcelController

--- src/main/java/xyz/stasiak/excelendpoint/ExcelController.java
+++ src/main/java/xyz/stasiak/excelendpoint/ExcelController.java
@@ -2,7 +2,7 @@ package xyz.stasiak.excelendpoint;
 
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
-import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,9 +10,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
-
-import java.io.IOException;
-import java.io.InputStream;
 
 @RestController
 @RequestMapping("/api")
@@ -26,20 +23,18 @@ class ExcelController {
 
     @GetMapping(value = "/excel/{filename}", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
     @ApiOperation(value = "Get Excel file with specified filename")
-    ResponseEntity<Resource> getWorkbook(@ApiParam(defaultValue = "spreadsheet") @PathVariable String filename)
-             throws IOException {
+    ResponseEntity<Resource> getWorkbook(@ApiParam(defaultValue = "spreadsheet") @PathVariable String filename) {
 
-        InputStream resourceStream = resourceProvider.getResourceStream()
+        ByteArrayResource resource = resourceProvider.getResource()
                                                      .orElseThrow(
                                                               () -> new ResponseStatusException(HttpStatus.NOT_FOUND,
                                                                                                 "File not found"));
-        InputStreamResource resource = new InputStreamResource(resourceStream);
 
         HttpHeaders headers = new HttpHeaders();
         headers.setContentDisposition(getContentDisposition(filename));
 
         return ResponseEntity.ok()
-                             .contentLength(resourceStream.available())
+                             .contentLength(resource.contentLength())
                              .contentType(MediaType.APPLICATION_OCTET_STREAM)
                              .headers(headers)
                              .body(resource);
