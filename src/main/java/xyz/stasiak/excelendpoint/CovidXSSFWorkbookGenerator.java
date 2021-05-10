fd1818026b14e778ed946ee8a550a54d40ab565d
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-05-10 21:28:59 +0200

    create class for generating workbook with covid data

---
+++ src/main/java/xyz/stasiak/excelendpoint/CovidXSSFWorkbookGenerator.java
@@ -0,0 +1,11 @@
+package xyz.stasiak.excelendpoint;
+
+import org.apache.poi.ss.usermodel.Workbook;
+
+class CovidXSSFWorkbookGenerator implements WorkbookGenerator {
+
+    @Override
+    public Workbook getWorkbook() {
+        return null;
+    }
+}
