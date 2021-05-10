b7c92c6ba6bde771cf0451518a718a308d530274
Dawid Stasiak <dawid.stasiak21@gmail.com>
2021-05-10 22:55:08 +0200

    add CovidRestClient field to covid workbook generator

--- src/main/java/xyz/stasiak/excelendpoint/CovidXSSFWorkbookGenerator.java
+++ src/main/java/xyz/stasiak/excelendpoint/CovidXSSFWorkbookGenerator.java
@@ -4,6 +4,12 @@ import org.apache.poi.ss.usermodel.Workbook;
 
 class CovidXSSFWorkbookGenerator implements WorkbookGenerator {
 
+    private final CovidRestClient covidRestClient;
+
+    CovidXSSFWorkbookGenerator(CovidRestClient covidRestClient) {
+        this.covidRestClient = covidRestClient;
+    }
+
     @Override
     public Workbook getWorkbook() {
         return null;
